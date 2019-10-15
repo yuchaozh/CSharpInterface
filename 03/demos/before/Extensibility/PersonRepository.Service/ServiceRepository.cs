62d6210d5fffb14750b2f31482d2a8d2b5abba6c
Yuchao Zhou <yuczho@microsoft.com>
2019-10-14 22:03:59 -0700

    finish implementing a service repository

---
+++ 03/demos/before/Extensibility/PersonRepository.Service/ServiceRepository.cs
@@ -0,0 +1,42 @@
+﻿using Newtonsoft.Json;
+using PersonRepository.Interface;
+using System;
+using System.Collections.Generic;
+using System.Net;
+
+namespace PersonRepository.Service
+{
+    public class ServiceRepository : IPersonRepository
+    {
+        WebClient client = new WebClient();
+        readonly string baseUri = "http://localhost:9874/api/people";
+
+
+        public void AddPerson(Person newPerson)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeletePerson(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Person> GetPeople()
+        {
+            string result = client.DownloadString(baseUri);
+            IEnumerable<Person> people = JsonConvert.DeserializeObject<IEnumerable<Person>>(result);
+            return people;
+        }
+
+        public Person GetPerson(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdatePerson(int id, Person updatedPerson)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
